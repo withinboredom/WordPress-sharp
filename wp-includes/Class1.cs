3a45769768fde9e25c8c0fed349772960fada454
Robert Landers <landers.robert@gmail.com>
2017-10-15 22:36:30 -0400

    Add project files.

---
+++ wp-includes/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wp_includes
+{
+    public class Class1
+    {
+    }
+}
